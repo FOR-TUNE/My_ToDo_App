e053ac3de7d63f225ff1f9647886bec24666fcbe
Fortune <fortune.onyema22@gmail.com>
2023-05-10 16:37:29 +0100

    Added custom color pallete

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color toDoCardColor = Colors.white;
+
+const Color toRed = Colors.red;
+const Color toPurple = Colors.deepPurpleAccent;
+
+const Color toBlack = Colors.black;
+const Color toGrey = Colors.grey;
